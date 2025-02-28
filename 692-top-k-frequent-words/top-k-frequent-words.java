0ae4600717bceb1631c4fb15ec99b4e42dd14be0
SHAURYA JAIN <125687256+shauryajain1@users.noreply.github.com>
2025-02-28 15:12:04 +0530

    Time: 7 ms (68.73%) | Memory: 44.1 MB (99.84%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String word:words){
+            map.put(word, map.getOrDefault(word, 0)+1);
+        }
+        List<Map.Entry<String, Integer>> l = new LinkedList<>();
+        for(Map.Entry<String, Integer> e:map.entrySet()){
+            l.add(e);
+        }
+        Collections.sort(l, new MyComparator());//just use our Comparator to sort
+        List<String> ans = new LinkedList<>();
+        for(int i = 0;i<=k-1;i++){
+            ans.add(l.get(i).getKey());
+        }
+        return ans;
+    }
+}
+/*
+Implement our own comparator for this problem, I will also use this Comparator in other methods(A little different in minHeap method).
+We can also use anonymous Comparaotr or Lambda function.
+*/
+class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+
+    public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2){
+        String word1 = e1.getKey();
+        int freq1 = e1.getValue();
+        String word2 = e2.getKey();
+        int freq2 = e2.getValue();
+        if(freq1!=freq2){
+            return freq2-freq1;
+        }
+        else {
+            return word1.compareTo(word2);
+        }
+    }
+}
