eb36ac70505ca7a8292b828829479b03b747929d
SHAURYA JAIN <125687256+shauryajain1@users.noreply.github.com>
2025-04-09 00:03:40 +0530

    Time: 1 ms (98.39%) | Memory: 44.3 MB (73.87%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int cnt = 0, temp;
-        List<Integer> list = new ArrayList<>();
-        for (int n : nums)
-            list.add(n);
-        while (true) {
-            HashMap<Integer, Integer> mpp = new HashMap<>();
-            temp = 0;
-            for (int num : list)
-                if (mpp.put(num, mpp.getOrDefault(num, 0) + 1) != null && mpp.get(num) == 2)
-                    temp++;
-            if (temp == 0)
-                break;
-            int removeCount = Math.min(3, list.size());
-            for (int i = 0; i < removeCount; i++)
-                list.remove(0);
-            cnt++;
-        }
-        return cnt;
+        int[] freq = new int[101];
+         Arrays.fill (freq, 0);
+
+         for(int i = nums.length - 1; i>=0; i--){
+            if(++freq[nums[i]] > 1)
+            return(int)
+            Math.ceil((double) (i + 1) / 3);
+         }
+         return 0;
     }
 }
+
+      
+   
+    
