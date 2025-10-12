f72721f6f4956d4237db132c2864b6a3a87f2b85
SHAURYA JAIN <125687256+shauryajain1@users.noreply.github.com>
2025-10-12 22:44:56 +0530

    Time: 3 ms (99.72%) | Memory: 57.2 MB (99.72%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,20 +1,12 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(0, n, costs, dp);
-    }
-
-    private int solve(int i, int n, int[] costs, int[] dp) {
-        if (i > n) return Integer.MAX_VALUE;  // invalid jump
-        if (i == n) return 0; // reached top
-
-        if (dp[i] != -1) return dp[i];
-
-        int oneStep = (costs[i] + 1) + solve(i + 1, n, costs, dp);
-        int twoStep = i + 2 <= n ? (costs[i + 1] + 4) + solve(i + 2, n, costs, dp) : Integer.MAX_VALUE;
-        int threeStep = i + 3 <= n ? (costs[i + 2] + 9) + solve(i + 3, n, costs, dp) : Integer.MAX_VALUE;
-
-        return dp[i] = Math.min(oneStep, Math.min(twoStep, threeStep));
+        int v0 = 0, v1 = 0, v2 = 0, v;
+        for (int c : costs) {
+            v = Math.min(Math.min(v0 + 9, v1 + 4), v2 + 1) + c;
+            v0 = v1;
+            v1 = v2;
+            v2 = v;
+        }
+        return v2;
     }
 }
